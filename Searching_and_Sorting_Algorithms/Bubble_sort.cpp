47f376360faf5c020ea3b56ea1554aa8e337c3ed
pk13055 <pkrockstar7@gmail.com>
2016-11-16 17:35:37 +0530

    Bubble sort changed.

--- Searching_and_Sorting_Algorithms/Bubble_sort.cpp
+++ Searching_and_Sorting_Algorithms/Bubble_sort.cpp
@@ -1,56 +1,42 @@
 #include<iostream>
-
-//
-// Bubble Sort
-//
-// Bubble_sort.cpp
-//
-//Bubble sort is (provably) the fastest sort available under a very specific circumstance. It
-//originally became well known primarily because it was one of the first algorithms (of any kind)
-//that was rigorously analyzed, and the proof was found that it was optimal under its limited
-//circumstance.
-//Complexity O(n^2)
-
+#define int unsigned long long int 
+/**
+*	Bubble Sort
+*
+*	Bubble_sort.cpp
+*
+*	Bubble sort is (provably) the fastest sort available under a very specific circumstance. It
+*	originally became well known primarily because it was one of the first algorithms (of any kind)
+*	that was rigorously analyzed, and the proof was found that it was optimal under its limited
+*	circumstance.
+*	Complexity O(n^2)
+**/
 using namespace std;
 
-void Bubble_sort (int arr[], int length) {
-  int maximum_element ;
-  int pointer;
-  for (int i = 0; i < length; i++) {
-    maximum_element = arr[0];
-    pointer = 0;
-    for (int j = 0; j < length - i; j++) {
-      if (arr[j] > maximum_element) {
-        maximum_element = arr[j];
-        pointer = j;
+void Bubble_sort (int arr[],int  length) {
+  for (int i=0; i<length;i++) 
+    for (int j=0;j<length-i-1;j++) 
+      if (arr[j] > arr[j+1]) {
+	      int temp=arr[j];
+	      arr[j]=arr[j+1];
+	      arr[j+1]=temp;
       }
-    }
-    int temp;
-    temp = arr[pointer];
-    arr[pointer] = arr[length - i-1];
-    arr[length - i-1] = temp;
-  }
 }
-
 void Display (int arr[], int length) {
-  for (int i = 0; i < length; i++) {
-    cout << "\nElement " << i+1 << " : " << arr[i] ;
-  }
-  cout << endl ;
+  for (int i=0;i<length;i++)
+    cout<<"\nElement "<<i+1<<"\t : "<<arr[i];
+  cout<<endl;
 }
 
-int main () {
+main () {
   int length;
-  cout << "Enter length of the array " ;
-  cin >> length;
+  cout<<"Enter length of the array " ;
+  cin>>length;
   int arr[length];
-  for (int i = 0; i < length; i++) {
-    cout << "\nEnter element number " << i+1 << " " ;
-    cin >> arr[i];
+  for (int i=0;i<length;i++) {
+    cout<<"\nEnter element "<<i+1<<" : ";
+    cin>>arr[i];
   }
-
   Bubble_sort(arr, length);
-  Display(arr, length);
-
-  return 0;
+  Display(arr,length);
 }
