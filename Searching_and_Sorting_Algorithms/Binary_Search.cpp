0f1273cab205c788378ddb4f15a34f0fb69b658f
VK10 <vivek.kaushal@outlook.com>
2016-11-16 16:00:14 +0530

    Binary Search Added (#2)
    * Binary Search Added
    
    * Binary Search modified
    
    * Binary Search modified, complexity added
    
    * Update Binary_Search.cpp

---
+++ Searching_and_Sorting_Algorithms/Binary_Search.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+//
+//
+// Binary Search
+//
+//
+// Binary_Search.pp
+//
+//
+//Binary Search is a search algorithm that is more optimised than linear search.
+//It significantly reduces the runtime of programs.
+//The following is a basic implementation of the Binary Search algorithm.
+//Complexity O(log n).
+
+using namespace std;
+
+void BinarySearch(int A[], int a, int b, int n) {
+	int start = a, end = b ;
+        if( n == ( A[start] + A[end] ) / 2) {
+              	cout << ( start + end ) / 2 ;
+                return;
+        }
+        else if( n > ( A[start] + A[end]) / 2 )
+                BinarySearch ( A, ((start+end)/2) + 1 , end, n ) ;
+
+        else
+                BinarySearch ( A , start , ( (start+end) / 2 ) - 1 , n) ;
+}
+
+int main() {
+        int count,i ;
+        cin >> count ;
+        int A[count] ;
+        for(i = 0; i < count; i++)
+                cin >> A[i] ;		//Takes the input array where Binary Search is to be executed
+        int n ;
+        cin >> n ;
+        BinarySearch(A,0,count-1,n) ;
+	cout << endl ;
+        return 0 ;
+}
+
