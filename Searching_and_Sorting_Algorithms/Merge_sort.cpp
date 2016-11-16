b17ecb9ad8ec7aa3041f343607683521d1dbf922
Swapnil Gupta <500swapnil@github.com>
2016-11-16 15:39:48 +0530

    Merge sort updated

--- Searching_and_Sorting_Algorithms/Merge_sort.cpp
+++ Searching_and_Sorting_Algorithms/Merge_sort.cpp
@@ -3,7 +3,9 @@
 //Merge Sort
 
 //Merge_Sort.cpp
+//
 //In computer science, mergesort is an efficient, comparison based algorithm
+//
 //It is a divide and conquer algorithm that produces a stable sort(preserves
 //input order of equal elements
 //It has 2 steps
@@ -16,65 +18,55 @@ using namespace std;
 // Merges two subarrays of arr[].
 // First subarray is arr[l..m]
 // Second subarray is arr[m+1..r]
-void merge(int arr[], int l, int m, int r)
-{
+void merge (int arr[], int l, int m, int r) {
 	int i, j, k;
 	int n1 = m - l + 1;
 	int n2 =  r - m;
 
-	/* create temp arrays */
+	//Create temp arrays
 	int L[n1], R[n2];
 
-	/* Copy data to temp arrays L[] and R[] */
+	//Copy data to temp arrays L[] and R[]
 	for (i = 0; i < n1; i++)
 		L[i] = arr[l + i];
 	for (j = 0; j < n2; j++)
 		R[j] = arr[m + 1+ j];
 
-	/* Merge the temp arrays back into arr[l..r]*/
+	// Merge the temp arrays back into arr[l..r]
 	i = 0; // Initial index of first subarray
 	j = 0; // Initial index of second subarray
 	k = l; // Initial index of merged subarray
-	while (i < n1 && j < n2)
-	{
-		if (L[i] <= R[j])
-		{
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
 			arr[k] = L[i];
 			i++;
 		}
-		else
-		{
+		else {
 			arr[k] = R[j];
 			j++;
 		}
 		k++;
 	}
 
-	/* Copy the remaining elements of L[], if there
-	   are any */
-	while (i < n1)
-	{
+	// Copy the remaining elements of L[], if there are any 
+	while (i < n1) {
 		arr[k] = L[i];
 		i++;
 		k++;
 	}
 
-	/* Copy the remaining elements of R[], if there
-	   are any */
-	while (j < n2)
-	{
+	// Copy the remaining elements of R[], if there are any
+	while (j < n2) {
 		arr[k] = R[j];
 		j++;
 		k++;
 	}
 }
 
-/* l is for left index and r is right index of the
-   sub-array of arr to be sorted */
-void Merge_sort(int arr[], int l, int r)
-{
-	if (l < r)
-	{
+// l is for left index and r is right index of the
+//   sub-array of arr to be sorted
+void Merge_sort (int arr[], int l, int r) {
+	if (l < r) {
 		// Same as (l+r)/2, but avoids overflow for
 		// large l and h
 		int m = l+(r-l)/2;
