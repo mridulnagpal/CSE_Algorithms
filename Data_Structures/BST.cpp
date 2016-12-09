0dd4eb09118639e67cf3bf7090af153aed8028c4
Animesh Das <animeshdas4883@gmail.com>
2016-12-09 20:28:44 +0530

    Corrected BST.cpp

--- Data_Structures/BST.cpp
+++ Data_Structures/BST.cpp
@@ -33,16 +33,16 @@ Node* root;
 class BST
 {
 	public:
-		void find(int, node **, node **);
-		void insert(node *, node*);
+		void find(int, Node **, Node **);
+		void insert(Node *, Node*);
 		void del(int);
-		void case_a(node *, node *);
-		void case_b(node *, node *);
-		void case_c(node *, node *);
-		void preorder(node *);
-		void inorder(node *);
-		void postorder(node *);
-		void display(node *, int);
+		void case_a(Node *, Node *);
+		void case_b(Node *, Node *);
+		void case_c(Node *, Node *);
+		void preorder(Node *);
+		void inorder(Node *);
+		void postorder(Node *);
+		void display(Node *, int);
 		BST(){ root=NULL; }
 };
 
